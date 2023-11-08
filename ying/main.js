311f904d2bd2ff0a7daf187d2fde1e073fda3e87
nguyennmt3009 <thaonguyen300996@gmail.com>
2023-11-08 13:26:01 +0900

    gay

--- ying/main.js
+++ ying/main.js
@@ -125,6 +125,22 @@ searchTxt.on('keyup', function () {
             img.height = 50;
             // add to resultImg
             resultImg.append(img);
+        } else {
+            var gayWords = searchGay(input[i]);
+            if (gayWords) {
+                for (var j = 0; j < gayWords.length; j++) {
+                    imgSrc = searchChar(gayWords[j]);
+                    if (imgSrc) {
+                        // create image
+                        var img = document.createElement('img');
+                        img.src = imgSrc;
+                        img.width = 50;
+                        img.height = 50;
+                        // add to resultImg
+                        resultImg.append(img);
+                    } 
+                }
+            }
         }
     }
     resultTxt.val(result);
@@ -138,3 +154,146 @@ function searchChar(char) {
     }
     return null;
 }
+
+function searchGay(gay) {
+    for (var i = 0; i < dictionaryGay.length; i++) {
+        if (dictionaryGay[i].gay == gay) {
+            return dictionaryGay[i].words;
+        }
+    }
+    return null;
+}
+
+////// Ying's Dictionary Gay Show //////
+var dictionaryGay = [
+    {'gay': 'hướng', 'words': ['trái', 'phải']},
+    {'gay': 'phương', 'words': ['lên', 'xuống']},
+];
+
+// local storage
+var dictionaryGayLocal = JSON.parse(localStorage.getItem('dictionaryGay'));
+if (dictionaryGayLocal != null) {
+    dictionaryGay = dictionaryGayLocal;
+}
+
+
+var dicGayTable = $('#dicGayTable');
+
+function renderGayTable() {
+    dicGayTable.empty();
+    for (var i = 0; i < dictionaryGay.length; i++) {
+        var row = '<tr>';
+        row += '<td>' + dictionaryGay[i].gay + '</td>';
+        row += '<td>';
+        for (var j = 0; j < dictionaryGay[i].words.length; j++) {
+            row += dictionaryGay[i].words[j] + ' ';
+        }
+        row += '</td>';
+        row += '<td><button class="btn btn-danger btn-sm" data-index="' + i + '">Delete</button></td>';
+        row += '</tr>';
+        dicGayTable.append(row);
+    }
+}
+
+renderGayTable();
+
+function saveToLocalStorageGay() {
+    localStorage.setItem('dictionaryGay', JSON.stringify(dictionaryGay));
+}
+
+
+////// Ying's Dictionary Gay Delete //////
+
+dicGayTable.on('click', '.btn-danger', function () {
+    var index = $(this).data('index');
+    dictionaryGay.splice(index, 1);
+    dicGayTable.empty();
+    renderGayTable();
+    saveToLocalStorageGay();
+});
+
+////// Ying's Dictionary Gay Tag //////
+
+
+const tagInput = document.getElementById('tagInput');
+const tagsContainer = document.getElementById('tagsContainer');
+const tagSuggestions = document.getElementById('tagSuggestions');
+
+let tags = [];
+let suggestions = [];
+
+for (var i = 0; i < dictionary.length; i++) {
+    suggestions.push(dictionary[i].char);
+}
+
+function displayTags() {
+    tagsContainer.innerHTML = '';
+    tags.forEach(tag => {
+        const tagElem = document.createElement('span');
+        tagElem.className = 'tag-item';
+        tagElem.textContent = tag;
+        const closeBtn = document.createElement('span');
+        closeBtn.className = 'close';
+        closeBtn.textContent = '×';
+        closeBtn.onclick = function () {
+            tags = tags.filter(t => t !== tag);
+            displayTags();
+        };
+        tagElem.appendChild(closeBtn);
+        tagsContainer.appendChild(tagElem);
+    });
+}
+
+tagInput.addEventListener('keyup', (e) => {
+    const inputText = e.target.value.trim();
+    tagSuggestions.innerHTML = '';
+    if (inputText) {
+        const filteredSuggestions = suggestions.filter(suggestion =>
+            suggestion.toLowerCase().includes(inputText.toLowerCase())
+        );
+        filteredSuggestions.forEach(suggestion => {
+            const div = document.createElement('div');
+            div.className = 'list-group-item list-group-item-action';
+            div.textContent = suggestion;
+            div.onclick = function () {
+                if (!tags.includes(suggestion)) {
+                    tags.push(suggestion);
+                    displayTags();
+                }
+                tagInput.value = '';
+                tagSuggestions.innerHTML = '';
+            };
+            tagSuggestions.appendChild(div);
+        });
+    }
+});
+
+displayTags();
+
+////// Ying's Dictionary Gay Add //////
+
+var addGayBtn = $('#addGayBtn');
+
+addGayBtn.on('click', function () {
+    removeAlert();
+    var gayWord = $('#gayTxt').val().toLowerCase();
+    console.log(gayWord);
+
+    var isExisting = dictionaryGay.some(function (entry) {
+        return entry.gay === gayWord;
+    });
+
+    if (!isExisting && gayWord !== '' && tags.length > 0) {
+        var newEntry = { 'gay': gayWord, 'words': tags };
+        dictionaryGay.push(newEntry);
+        console.log(dictionaryGay);
+        $('#gayTxt').val('');
+        tags = [];
+        displayTags();
+        renderGayTable();
+        saveToLocalStorageGay();
+    } else {
+        addAlert();
+    }
+});
+
